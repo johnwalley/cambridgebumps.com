26b8c14f46d7b245db300da9d1d85b6bae13d63a
johnwalley <john@walley.org.uk>
2022-07-13 14:11:52 +0100

    chore: latest cheat sheets

--- src/pages/cheat-sheets.jsx
+++ src/pages/cheat-sheets.jsx
@@ -31,12 +31,12 @@ export default function CheatSheets() {
             <h2 className="text-2xl font-extrabold tracking-tight text-gray-900">
               Guides to the crews taking part.
             </h2>
-            <p>Identify crew colours, statistics and more.</p>
+            <p>Identify crew colours and look up statistics, division times and more.</p>
 
             <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
               {products.map((product) => (
                 <div key={product.id} className="group relative">
-                  <div className="min-h-80 aspect-w-210 aspect-h-297 lg:aspect-none w-full overflow-hidden rounded-md bg-gray-200 group-hover:opacity-75 lg:h-80">
+                  <div className="min-h-80 aspect-w-210 aspect-h-297 lg:aspect-none w-full overflow-hidden rounded-md bg-gray-200 group-hover:opacity-75 ">
                     <Image
                       src={townBumpsCheatSheetImage}
                       alt=""
