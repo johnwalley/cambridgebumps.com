853b0e994c05c2848cb81a1979c9e1e8df2ad3cf
johnwalley <john@walley.org.uk>
2022-07-17 19:34:49 +0100

    Banner

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,15 +1,21 @@
 import Head from "next/head";
+import Link from "next/link";
+
+import { useState } from "react";
 
 import { CallToAction } from "@/components/CallToAction";
 import { Faqs } from "@/components/Faqs";
 import { Footer } from "@/components/Footer";
 import { Header } from "@/components/Header";
 import { Hero } from "@/components/Hero";
-import { PrimaryFeatures } from "@/components/PrimaryFeatures";
 import { SecondaryFeatures } from "@/components/SecondaryFeatures";
 import { Testimonials } from "@/components/Testimonials";
 
+import { SpeakerphoneIcon, XIcon } from "@heroicons/react/outline";
+
 export default function Home() {
+  const [showBanner, setShowBanner] = useState(true);
+
   return (
     <>
       <Head>
@@ -25,6 +31,50 @@ export default function Home() {
         <Faqs />
       </main>
       <Footer />
+      {showBanner && (
+        <div className="fixed inset-x-0 bottom-0 pb-2 sm:pb-5">
+          <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+            <div className="rounded-lg bg-cambridge p-2 shadow-lg sm:p-3">
+              <div className="flex flex-wrap items-center justify-between">
+                <div className="flex w-0 flex-1 items-center">
+                  <span className="flex rounded-lg bg-cambridge p-2">
+                    <SpeakerphoneIcon
+                      className="h-6 w-6 text-white"
+                      aria-hidden="true"
+                    />
+                  </span>
+                  <p className="ml-3 truncate font-medium text-white">
+                    <span className="md:hidden">CRA Town Bumps this week!</span>
+                    <span className="hidden md:inline">
+                      Cambridge Rowing Association Town Bumps 2022 taking place
+                      this week
+                    </span>
+                  </p>
+                </div>
+                <div className="order-3 mt-2 w-full flex-shrink-0 sm:order-2 sm:mt-0 sm:w-auto">
+                  <Link href="/latest/town">
+                    <a className="flex items-center justify-center rounded-md border border-transparent bg-white px-4 py-2 text-sm font-medium text-cambridge shadow-sm">
+                      Latest results
+                    </a>
+                  </Link>
+                </div>
+                <div className="order-2 flex-shrink-0 sm:order-3 sm:ml-2">
+                  <button
+                    type="button"
+                    className="-mr-1 flex rounded-md p-2 hover:bg-cambridge focus:outline-none focus:ring-2 focus:ring-white"
+                    onClick={() => {
+                      setShowBanner(false);
+                    }}
+                  >
+                    <span className="sr-only">Dismiss</span>
+                    <XIcon className="h-6 w-6 text-white" aria-hidden="true" />
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
     </>
   );
 }
