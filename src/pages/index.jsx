373c564e58d25e620019b7c1737b9c8cf68bbd08
johnwalley <john@walley.org.uk>
2023-05-21 13:33:57 +0100

    fix: page title

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -12,6 +12,7 @@ import { SecondaryFeatures } from "@/components/SecondaryFeatures";
 import { Testimonials } from "@/components/Testimonials";
 
 import { SpeakerphoneIcon, XIcon } from "@heroicons/react/outline";
+import { i18n } from "../i18n";
 
 export default function Home() {
   const [showBanner, setShowBanner] = useState(false);
@@ -19,7 +20,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Cambridge Bumps</title>
+        <title>{i18n.name} Bumps</title>
         <meta
           name="description"
           content="Cambridge and Oxford Bumps results and charts."
@@ -34,10 +35,10 @@ export default function Home() {
       {showBanner && (
         <div className="fixed inset-x-0 bottom-0 pb-2 sm:pb-5">
           <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-            <div className="rounded-lg bg-cambridge p-2 shadow-lg sm:p-3">
+            <div className="bg-cambridge rounded-lg p-2 shadow-lg sm:p-3">
               <div className="flex flex-wrap items-center justify-between">
                 <div className="flex w-0 flex-1 items-center">
-                  <span className="flex rounded-lg bg-cambridge p-2">
+                  <span className="bg-cambridge flex rounded-lg p-2">
                     <SpeakerphoneIcon
                       className="h-6 w-6 text-white"
                       aria-hidden="true"
@@ -53,7 +54,7 @@ export default function Home() {
                 </div>
                 <div className="order-3 mt-2 w-full flex-shrink-0 sm:order-2 sm:mt-0 sm:w-auto">
                   <Link href="/latest/town">
-                    <a className="flex items-center justify-center rounded-md border border-transparent bg-white px-4 py-2 text-sm font-medium text-cambridge shadow-sm">
+                    <a className="text-cambridge flex items-center justify-center rounded-md border border-transparent bg-white px-4 py-2 text-sm font-medium shadow-sm">
                       Latest results
                     </a>
                   </Link>
@@ -61,7 +62,7 @@ export default function Home() {
                 <div className="order-2 flex-shrink-0 sm:order-3 sm:ml-2">
                   <button
                     type="button"
-                    className="-mr-1 flex rounded-md p-2 hover:bg-cambridge focus:outline-none focus:ring-2 focus:ring-white"
+                    className="hover:bg-cambridge -mr-1 flex rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-white"
                     onClick={() => {
                       setShowBanner(false);
                     }}
