47f62d0439fff430c37a5e1a5376288673420d2a
johnwalley <john@walley.org.uk>
2023-06-14 22:34:45 +0100

    feat: better footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,55 +1,76 @@
+import { Container } from "@/components/Container";
 import Link from "next/link";
 
-import { Container } from "@/components/Container";
-import { Logo } from "@/components/Logo";
+const navigation = {
+  main: [
+    { name: "Latest results", href: "/latest" },
+    { name: "Historical charts", href: "/history" },
+    { name: "Cheat sheets", href: "/cheat-sheets" },
+    { name: "How do Bumps work?", href: "/about" },
+    { name: "Vocabulary", href: "/vocabulary" },
+  ],
+  social: [
+    {
+      name: "Twitter",
+      href: "https://twitter.com/johnmwalley",
+      icon: (props) => (
+        <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
+          <path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84" />
+        </svg>
+      ),
+    },
+    {
+      name: "GitHub",
+      href: "https://github.com/johnwalley/bumps-results",
+      icon: (props) => (
+        <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
+          <path
+            fillRule="evenodd"
+            d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z"
+            clipRule="evenodd"
+          />
+        </svg>
+      ),
+    },
+  ],
+};
 
 export function Footer() {
   return (
     <footer className="bg-slate-50">
       <Container>
-        <div className="py-16">
-          <Logo className="mx-auto h-10 w-auto" />
-          <nav className="mt-10 text-sm" aria-label="quick links">
-            <ul className="-my-1 flex justify-center space-x-6">
-              <li>
-                <Link href="/latest">
-                  <a className="rounded-lg px-2 py-1 text-slate-700 hover:bg-slate-100 hover:text-slate-900">
-                    Latest results
-                  </a>
-                </Link>
-              </li>
-              <li>
-                <Link href="/history">
-                  <a className="rounded-lg px-2 py-1 text-slate-700 hover:bg-slate-100 hover:text-slate-900">
-                    Historical charts
-                  </a>
-                </Link>
-              </li>
-              <li>
-                <Link href="/cheat-sheets">
-                  <a className="rounded-lg px-2 py-1 text-slate-700 hover:bg-slate-100 hover:text-slate-900">
-                    Cheat sheets
-                  </a>
-                </Link>
-              </li>
-              <li>
-                <Link href="/about">
-                  <a className="rounded-lg px-2 py-1 text-slate-700 hover:bg-slate-100 hover:text-slate-900">
-                    How do Bumps work?
-                  </a>
-                </Link>
-              </li>
-              <li>
-                <Link href="/vocabulary">
-                  <a className="rounded-lg px-2 py-1 text-slate-700 hover:bg-slate-100 hover:text-slate-900">
-                    Vocabulary
-                  </a>
-                </Link>
-              </li>
-            </ul>
-          </nav>
+        <nav
+          className="-mb-6 columns-2 pt-8 sm:flex sm:justify-center sm:space-x-12"
+          aria-label="Footer"
+        >
+          {navigation.main.map((item) => (
+            <div key={item.name} className="pb-6">
+              <Link
+                href={item.href}
+                className="text-sm leading-6 text-gray-600 hover:text-gray-900"
+              >
+                {item.name}
+              </Link>
+            </div>
+          ))}
+        </nav>
+        <div className="mt-10 flex justify-center space-x-10">
+          {navigation.social.map((item) => (
+            <a
+              key={item.name}
+              href={item.href}
+              className="text-gray-400 hover:text-gray-500"
+            >
+              <span className="sr-only">{item.name}</span>
+              <item.icon className="h-6 w-6" aria-hidden="true" />
+            </a>
+          ))}
         </div>
-        <div className="flex flex-col items-center border-t border-slate-400/10 py-10 sm:flex-row-reverse sm:justify-between">
+        <p className="mt-10 text-center text-xs leading-5 text-gray-500">
+          Copyright &copy; {new Date().getFullYear()} John Walley. All rights
+          reserved
+        </p>
+        {/*         <div className="flex flex-col items-center border-t border-slate-400/10 py-10 sm:flex-row-reverse sm:justify-between">
           <div className="flex space-x-6">
             <a className="group" href="https://twitter.com/johnmwalley">
               <span className="sr-only">Cambridge Bumps on Twitter</span>
@@ -77,7 +98,7 @@ export function Footer() {
             Copyright &copy; {new Date().getFullYear()} John Walley. All rights
             reserved.
           </p>
-        </div>
+        </div> */}
       </Container>
     </footer>
   );
