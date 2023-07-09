490ce7b285d3ca07066239717bdf4e77670dcf47
johnwalley <john@walley.org.uk>
2023-07-09 20:49:56 +0100

    Statistics

---
+++ src/pages/stats/[event]/[gender]/index.jsx
@@ -0,0 +1,74 @@
+import Head from "next/head";
+import Link from "next/link";
+import { useRouter } from "next/router";
+
+import { Header } from "@/components/Header";
+
+import { genders, longNames } from "../../../../constants";
+import { i18n } from "../../../../i18n";
+
+const stats = [
+  {
+    id: "movup",
+    name: "Biggest rise",
+  },
+  {
+    id: "movdo",
+    name: "Biggest fall",
+  },
+  {
+    id: "nhead",
+    name: "Number of headships",
+  },
+];
+
+export default function Statistics() {
+  const router = useRouter();
+  const { event, gender } = router.query;
+
+  return (
+    <>
+      <Head>
+        <title>{`Statistics - ${longNames[event]} - ${i18n.name} Bumps`}</title>
+      </Head>
+      <Header />
+
+      <main>
+        <div className="bg-white">
+          <div className="mx-auto max-w-7xl py-2 px-4 sm:px-6 lg:px-8 lg:py-4">
+            <div className="grid grid-cols-1 gap-12 lg:grid-cols-3 lg:gap-8">
+              <div className="space-y-5 sm:space-y-4">
+                <h2 className="text-3xl font-extrabold tracking-tight sm:text-4xl">
+                  Statistics
+                </h2>
+                <h3 className="text-2xl font-extrabold tracking-tight sm:text-3xl">
+                  {longNames[event]} - {gender}
+                </h3>
+              </div>
+              <div className="lg:col-span-2">
+                <ul
+                  role="list"
+                  className="space-y-12 sm:grid sm:grid-cols-2 sm:gap-12 sm:space-y-0 lg:gap-x-8"
+                >
+                  {stats.map((stat) => (
+                    <li key={stat.name}>
+                      <Link href={`/stats/${event}/${gender}/${stat.id}`}>
+                        <a>
+                          <div className="flex items-center space-x-4 lg:space-x-6">
+                            <div className="space-y-1 text-lg font-medium leading-6">
+                              <h3>{stat.name}</h3>
+                            </div>
+                          </div>
+                        </a>
+                      </Link>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
+    </>
+  );
+}
---
+++ src/pages/stats/[event]/[gender]/nhead/index.jsx
@@ -0,0 +1,92 @@
+import Head from "next/head";
+import { useRouter } from "next/router";
+
+import { Header } from "@/components/Header";
+
+import { longGenders, longNames } from "../../../../../constants";
+import { i18n } from "../../../../../i18n";
+
+export default function Statistics({ data }) {
+  const router = useRouter();
+
+  const { event, gender } = router.query;
+
+  if (!data) {
+    return <p>Loading</p>;
+  }
+
+  return (
+    <>
+      <Head>
+        <title>{`Statistics - ${longNames[event]} - ${longGenders[gender]} - ${i18n.name} Bumps`}</title>
+      </Head>
+      <Header />
+
+      <main>
+        <div className="mx-auto max-w-7xl py-2 px-4 sm:px-6 lg:px-8 lg:py-4">
+          <div className="flex flex-col items-center align-middle">
+            <h1>Number of headships</h1>
+            <div>
+              Number of times finished Head of the River (NHd) in {longNames[event]}, and
+              year of last Headship
+            </div>
+            <table>
+              <thead>
+                <tr>
+                  <th>#</th>
+                  <th>NHd</th>
+                  <th>Year</th>
+                  <th>Crews</th>
+                </tr>
+              </thead>
+              <tbody>
+                {data.map((d, i) => (
+                  <tr key={i}>
+                    <td>{i + 1}</td>
+                    <td>{d.count}</td>
+                    <td>{d.year}</td>
+                    <td>{d.crew}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </main>
+    </>
+  );
+}
+
+export async function getStaticPaths() {
+  return {
+    paths: [
+      { params: { event: "eights", gender: "men" } },
+      { params: { event: "eights", gender: "women" } },
+      { params: { event: "lents", gender: "men" } },
+      { params: { event: "lents", gender: "women" } },
+      { params: { event: "mays", gender: "men" } },
+      { params: { event: "mays", gender: "women" } },
+      { params: { event: "torpids", gender: "men" } },
+      { params: { event: "torpids", gender: "women" } },
+      { params: { event: "town", gender: "men" } },
+      { params: { event: "town", gender: "women" } },
+    ],
+    fallback: true,
+  };
+}
+
+export async function getStaticProps(context) {
+  const { event, gender } = context.params;
+
+  const res = await fetch(
+    `https://api.cambridgebumps.com/api/stats/nhead?event=${event}&gender=${gender}`
+  );
+
+  const data = await res.json();
+
+  return {
+    props: {
+      data,
+    },
+  };
+}
