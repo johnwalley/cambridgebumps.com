d4cf8bfc790695e0ff1bc4f3caff688c546944c5
johnwalley <john@walley.org.uk>
2024-06-26 16:10:39 +0100

    feat: add sitemap

---
+++ src/app/sitemap.ts
@@ -0,0 +1,26 @@
+import { MetadataRoute } from "next";
+
+const BASE_URL = process.env.BASE_URL;
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: `${BASE_URL}`,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 1,
+    },
+    {
+      url: `${BASE_URL}/about`,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
+      url: `${BASE_URL}/charts`,
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.5,
+    },
+  ];
+}
