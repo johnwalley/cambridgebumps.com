432246b6fdebde5c000b1ae46c208939e5bbcbdf
johnwalley <john@walley.org.uk>
2025-05-26 17:39:22 +0100

    Add Eights 2025 start order

--- src/components/announcement.tsx
+++ src/components/announcement.tsx
@@ -6,12 +6,12 @@ import { Separator } from "@/components/ui/separator";
 export function Announcement() {
   return (
     <Link
-      href="/charts/lents/women/2025"
+      href="/charts/eights/women/2025"
       className="inline-flex items-center rounded-lg bg-muted px-3 py-1 text-sm font-medium"
     >
-      <BellIcon className="h-4 w-4" />{" "}
+      <BellIcon className="h-4 w-4" />
       <Separator className="mx-2 h-4" orientation="vertical" />{" "}
-      <span>Lents 2025</span>
+      <span>Summer Eights 2025</span>
       <ArrowRightIcon className="ml-1 h-4 w-4" />
     </Link>
   );
