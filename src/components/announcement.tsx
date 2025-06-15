4a8ce46843b5efdcf93dd70f13fc7f8a25112237
johnwalley <john@walley.org.uk>
2025-06-15 11:21:29 +0100

    Prepare for Mays 2025

--- src/components/announcement.tsx
+++ src/components/announcement.tsx
@@ -1,17 +1,16 @@
-import { ArrowRightIcon } from "@radix-ui/react-icons";
-import { BellIcon } from "@radix-ui/react-icons";
-import Link from "next/link";
 import { Separator } from "@/components/ui/separator";
+import { ArrowRightIcon, BellIcon } from "@radix-ui/react-icons";
+import Link from "next/link";
 
 export function Announcement() {
   return (
     <Link
-      href="/charts/eights/women/2025"
+      href="/charts/mays/women/2025"
       className="inline-flex items-center rounded-lg bg-muted px-3 py-1 text-sm font-medium"
     >
       <BellIcon className="h-4 w-4" />
       <Separator className="mx-2 h-4" orientation="vertical" />{" "}
-      <span>Summer Eights 2025</span>
+      <span>May Bumps 2025</span>
       <ArrowRightIcon className="ml-1 h-4 w-4" />
     </Link>
   );
