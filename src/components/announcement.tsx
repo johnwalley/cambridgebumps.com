1425f19e7bf476094d19abd6458138d6c09f3960
johnwalley <john@walley.org.uk>
2025-07-09 15:13:27 +0100

    Update banner

--- src/components/announcement.tsx
+++ src/components/announcement.tsx
@@ -5,12 +5,12 @@ import Link from "next/link";
 export function Announcement() {
   return (
     <Link
-      href="/charts/mays/women/2025"
+      href="/charts/town/women/2025"
       className="inline-flex items-center rounded-lg bg-muted px-3 py-1 text-sm font-medium"
     >
       <BellIcon className="h-4 w-4" />
       <Separator className="mx-2 h-4" orientation="vertical" />{" "}
-      <span>May Bumps 2025</span>
+      <span>Town Bumps 2025</span>
       <ArrowRightIcon className="ml-1 h-4 w-4" />
     </Link>
   );
