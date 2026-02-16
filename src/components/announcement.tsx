efd207fe7a70e0d238bdc1f9e6d95c9162e70522
John Walley <john@walley.org.uk>
2026-02-16 17:24:54 +0000

    Add empty lents 2026 results

--- src/components/announcement.tsx
+++ src/components/announcement.tsx
@@ -5,12 +5,12 @@ import Link from "next/link";
 export function Announcement() {
   return (
     <Link
-      href="/charts/town/women/2025"
+      href="/charts/lents/women/2026"
       className="inline-flex items-center rounded-lg bg-muted px-3 py-1 text-sm font-medium"
     >
       <BellIcon className="h-4 w-4" />
       <Separator className="mx-2 h-4" orientation="vertical" />{" "}
-      <span>Town Bumps 2025</span>
+      <span>Lent Bumps 2026</span>
       <ArrowRightIcon className="ml-1 h-4 w-4" />
     </Link>
   );
