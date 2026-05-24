5a959bab5e85d6127c14cdb1e85b2d8f244b9849
John Walley <john@walley.org.uk>
2026-05-24 20:13:20 +0100

    Update announcement.tsx

--- src/components/announcement.tsx
+++ src/components/announcement.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 export function Announcement() {
   return (
     <Link
-      href="/charts/lents/women/2026"
+      href="/charts/eights/women/2026"
       className="inline-flex items-center rounded-lg bg-muted px-3 py-1 text-sm font-medium"
     >
       <BellIcon className="h-4 w-4" />
